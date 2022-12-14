7faf2ab534f131d34c51da8343ab185bb8e95723
Logan-Martin <lmartin27@charlottesvilleschools.org>
2022-12-14 11:12:09 -0500

    h

--- Code/AirHockeyTableCode.py
+++ Code/AirHockeyTableCode.py
@@ -10,6 +10,7 @@ led1 = digitalio.DigitalInOut(board.GP19)
 led1.direction = digitalio.Direction.OUTPUT
 
 scoreNeededToWinGame = 7
+scoreingDebounce = False
 
 player1 = {
     "score": 1
@@ -18,13 +19,21 @@ player2 = {
     "score": 0
 }
 
+player1Button = digitalio.DigitalInOut(board.GP2) # Button stuff
+player1Button.direction = digitalio.Direction.INPUT
+player1Button.pull = digitalio.Pull.DOWN
+
+player2Button = digitalio.DigitalInOut(board.GP3) # Button stuff
+player2Button.direction = digitalio.Direction.INPUT
+player2Button.pull = digitalio.Pull.DOWN
+
 def playerWonFunction(whoWon):
     print(str(whoWon) + " won the game!")
 
 
 def playerScoredFunction(whoScored):
-    if whoScored.score + 1 != scoreNeededToWinGame:
-        whoScored.score = whoScored.score + 1
+    if whoScored["score"] + 1 != scoreNeededToWinGame:
+        whoScored["score"] = whoScored["score"] + 1
     else:
         print("Winning point scored.")
         playerWonFunction(whoScored)
@@ -32,8 +41,8 @@ def playerScoredFunction(whoScored):
 
 def resetScoreFunction():
     print("Resetting Score.")
-    player1Score = 0
-    player2Score = 0
+    player1["score"] = 0
+    player1["score"] = 0
 
 while True:
     time.sleep(0.01)
@@ -41,5 +50,14 @@ while True:
     if resetButton.value == True and player1["score"] + player2["score"] != 0 : # When player press button, and combined score does not equal 0, then reset score. (Maybe make something for protecting the score?)
        print("Reseting score, maybe.")
 
+    if player1Button.value == True and scoreingDebounce == False:
+        scoreingDebounce = True
+        playerScoredFunction(player1)
+        time.sleep(0.05)
+        scoreingDebounce = False
 
-     
+    if player2Button.value == True and scoreingDebounce == False:
+        scoreingDebounce = True
+        playerScoredFunction(player2)
+        time.sleep(0.05)
+        scoreingDebounce = False
