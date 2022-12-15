961663f4574b036918ec47c5279dc41617864e2b
Logan-Martin <lmartin27@charlottesvilleschools.org>
2022-12-15 12:11:11 -0500

    haha code go brrrrr

--- Code/AirHockeyTableCode.py
+++ Code/AirHockeyTableCode.py
@@ -5,12 +5,15 @@ import time
 resetButton = digitalio.DigitalInOut(board.GP18) # Button stuff
 resetButton.direction = digitalio.Direction.INPUT
 resetButton.pull = digitalio.Pull.DOWN
+resetButtonWasPressed = False
 
 led1 = digitalio.DigitalInOut(board.GP19)
 led1.direction = digitalio.Direction.OUTPUT
 
 scoreNeededToWinGame = 7
-scoreingDebounce = False
+
+scoringDebounceForPlayer1 = False
+scoringDebounceForPlayer2 = False
 
 player1 = {
     "score": 1
@@ -42,24 +45,29 @@ def playerScoredFunction(whoScored):
 def resetScoreFunction():
     print("Resetting Score.")
     player1["score"] = 0
-    player1["score"] = 0
+    player2["score"] = 0
 
 while True:
     time.sleep(0.01)
     led1.value = True
-    if resetButton.value == True and player1["score"] + player2["score"] != 0 : # When player press button, and combined score does not equal 0, then reset score. (Maybe make something for protecting the score?)
-       print("Reseting score, maybe.")
+    if resetButton.value == True and player1["score"] + player2["score"] != 0 and resetButtonWasPressed == False: # When player press button, and combined score does not equal 0, then reset score. (Maybe make something for protecting the score?)
+       resetButtonWasPressed = True
+       resetScoreFunction()
+    if resetButton.value == False and resetButtonWasPressed == True:
+       resetButtonWasPressed = False
 
-    if player1Button.value == True and scoreingDebounce == False:
-        scoreingDebounce = True
+    if player1Button.value == True and scoringDebounceForPlayer1 == False:
+        scoringDebounceForPlayer1 = True
         print("Player1 scored!")
         playerScoredFunction(player1)
-        time.sleep(0.05)
-        scoreingDebounce = False
+        print("Score: " + "P1 - " + str(player1["score"]) + ", " + "P2 - " + str(player2["score"]))
+    if player1Button.value == False and scoringDebounceForPlayer1 == True:
+        scoringDebounceForPlayer1 = False
 
-    if player2Button.value == True and scoreingDebounce == False:
-        scoreingDebounce = True
+    if player2Button.value == True and scoringDebounceForPlayer2 == False:
+        scoringDebounceForPlayer2 = True
         print("Player2 scored!")
         playerScoredFunction(player2)
-        time.sleep(0.05)
-        scoreingDebounce = False
+        print("Score: " + "P1 - " + str(player1["score"]) + ", " + "P2 - " + str(player2["score"]))
+    if player2Button.value == False and scoringDebounceForPlayer2 == True:
+        scoringDebounceForPlayer2 = False
