126bab8a50730eebbf918319fc3d6d34fbfc4dbe
Logan-Martin <lmartin27@charlottesvilleschools.org>
2023-01-04 09:49:09 -0500

    sgs

--- Code/AirHockeyTableCode.py
+++ Code/AirHockeyTableCode.py
@@ -15,20 +15,17 @@ someoneWonTheGame = False
 scoringDebounceForPlayer1 = False
 scoringDebounceForPlayer2 = False
 
-debounceForWinning = 0
-## 0 = False, 1 == True
-
 player1 = {
     "name" : "player1", ## maybe let people enter their own names?
     "score": 0,
     "winCount" : 0,
-    "playerScoredWhatTheyNeedToWin" : False
+    "playerWonThisRound" : False
 }
 player2 = {
     "name" : "player2",
     "score": 0,
     "winCount" : 0,
-    "playerScoredWhatTheyNeedToWin" : False
+    "playerWonThisRound" : False
 }
 
 player1Button = digitalio.DigitalInOut(board.GP2) # Button stuff
@@ -43,16 +40,18 @@ def playerWonFunction(whoScored):
     print(str(whoScored["name"]) + " won the game!")
     whoScored["winCount"] = whoScored["winCount"] + 1
 
-def playerScoredFunction(whoScored,debounceForWinning):
-    if whoScored["score"] + 1 != scoreNeededToWinGame:
-        whoScored["score"] = whoScored["score"] + 1
-    else:
-        if player1["playerScoredWhatTheyNeedToWin"] == False and player2["playerScoredWhatTheyNeedToWin"] == False: ## nobody won yet
-            whoScored["playerScoredWhatTheyNeedToWin"] = True
+def playerScoredFunction(whoScored):
+    if player1["playerWonThisRound"] == False and player2["playerWonThisRound"] == False: ## if nobody won yet
+
+        if whoScored["score"] + 1 != scoreNeededToWinGame:
+            whoScored["score"] = whoScored["score"] + 1
+        else:
+            whoScored["playerWonThisRound"] = True
             whoScored["score"] = whoScored["score"] + 1
             playerWonFunction(whoScored)
+    else:
+        print("Someone has already won. Reset score to play again.")
             
-
 def resetScoreFunction():
     print("Score Reset.")
     player1["score"] = 0
@@ -61,22 +60,22 @@ def resetScoreFunction():
 def setScoreToWinFunction():
     ## scoreNeededToWinGame = scoreNeededToWinGame - 1
     ## scoreNeededToWinGame = scoreNeededToWinGame + 1
-    print("Haha")
+    print("Haha no.")
 
 while True:
     time.sleep(0.01)
     led1.value = True
     if resetButton.value == True and player1["score"] + player2["score"] != 0 and resetButtonWasPressed == False: # When player press button, and combined score does not equal 0, then reset score. (Maybe make something for protecting the score?)
        resetButtonWasPressed = True
-       player1["playerScoredWhatTheyNeedToWin"] = False
+       player1["playerWonThisRound"] = False
        resetScoreFunction()
     if resetButton.value == False and resetButtonWasPressed == True:
        resetButtonWasPressed = False
 
-    if player1Button.value == True and scoringDebounceForPlayer1 == False and player1["playerScoredWhatTheyNeedToWin"] == False:
+    if player1Button.value == True and scoringDebounceForPlayer1 == False and player1["playerWonThisRound"] == False:
         scoringDebounceForPlayer1 = True
         print("Player1 scored!")
-        playerScoredFunction(player1,debounceForWinning)
+        playerScoredFunction(player1)
         print("Score: " + "P1 - " + str(player1["score"]) + ", " + "P2 - " + str(player2["score"]))
     if player1Button.value == False and scoringDebounceForPlayer1 == True:
         scoringDebounceForPlayer1 = False
@@ -84,7 +83,7 @@ while True:
     if player2Button.value == True and scoringDebounceForPlayer2 == False and player2["playerScoredWhatTheyNeedToWin"] == False:
         scoringDebounceForPlayer2 = True
         print("Player2 scored!")
-        playerScoredFunction(player2,debounceForWinning)
+        playerScoredFunction(player2)
         print("Score: " + "P1 - " + str(player1["score"]) + ", " + "P2 - " + str(player2["score"]))
     if player2Button.value == False and scoringDebounceForPlayer2 == True:
         scoringDebounceForPlayer2 = False
