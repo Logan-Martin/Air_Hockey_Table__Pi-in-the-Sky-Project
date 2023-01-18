a564cd3b37df8ed2aa4e39874ae12c437e42c788
Logan-Martin <lmartin27@charlottesvilleschools.org>
2023-01-18 10:19:57 -0500

    ah

--- Code/AirHockeyTableCode.py
+++ Code/AirHockeyTableCode.py
@@ -21,25 +21,8 @@ lcd = LCD(interface, num_rows=rows, num_cols=cols)
 
 # Address for Distance Sensor: ['0x29']
 
-# import time # type: ignore
-# import board # type: ignore
-# import busio  # type: ignore
+distanceI2C = busio.I2C(board.GP15,board.GP14) 
 
-# i2c = busio.I2C(board.GP15,board.GP14) 
-
-# while not i2c.try_lock():
-#    pass
-
-# try:
-#    while True:
-#        print(
-#            "I2C addresses found:",
-#            [hex(device_address) for device_address in i2c.scan()],
-#        )
-#        time.sleep(2)
-
-# finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
-#    i2c.unlock()
 
 resetButton = digitalio.DigitalInOut(board.GP18) # Button stuff
 resetButton.direction = digitalio.Direction.INPUT
