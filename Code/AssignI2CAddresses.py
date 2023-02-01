19804f0dc7b47ddbff4b17f6b918143550d8723d
Logan-Martin <lmartin27@charlottesvilleschools.org>
2023-02-01 09:38:42 -0500

    aaaaa

--- Code/AssignI2CAddresses.py
+++ Code/AssignI2CAddresses.py
@@ -1,57 +1,50 @@
-import time
-import board # type: ignore
-import busio # type: ignore
-from digitalio import DigitalInOut # type: ignore
-import adafruit_vl53l0x # type: ignore
 
-print("Check 0!")
+import time  # type: ignore
+import board  # type: ignore
+import busio  # type: ignore
 
-i2c = busio.I2C(board.GP15,board.GP14) 
+from digitalio import DigitalInOut
+from adafruit_vl53l0x import VL53L0X
 
-print("Check 1!")
+i2c = busio.I2C(board.GP15,board.GP14) # declare the singleton variable for the default I2C bus
 
-shutdownPin_Table = [
+# declare the digital output pins connected to the "SHDN" pin on each VL53L0X sensor
+xshut = [
     DigitalInOut(board.GP11), # 2
     DigitalInOut(board.GP10), # 1
     # add more VL53L0X sensors by defining their SHDN pins here
 ]
 
-table_of_distance_sensors = [
-
-]
-
-for shutdown_pin in shutdownPin_Table:
+for power_pin in xshut:
     # make sure these pins are a digital output, not a digital input
-    shutdown_pin.switch_to_output(value=False)
-    print("Check 2!")
+    power_pin.switch_to_output(value=False)
+    # These pins are active when Low, meaning:
+    #   if the output signal is LOW, then the VL53L0X sensor is off.
+    #   if the output signal is HIGH, then the VL53L0X sensor is on.
+# all VL53L0X sensors are now off
 
-for i, v in enumerate(shutdownPin_Table):
-    print(str(i) + " From shutdownPin_Table.")
-    shutdown_pin.value = True # turn on the VL53L0X to allow hardware check
-    if i == 1:
-        # default address is 0x29. Change that to something else
-        distance_sensor_B = adafruit_vl53l0x.VL53L0X(i2c)
-        distance_sensor_B.set_address(0x30)  # address assigned should NOT be already in use
-        table_of_distance_sensors.insert(1,distance_sensor_B)
-        print("Check 4!")
-    else:
-        distance_sensor_A = adafruit_vl53l0x.VL53L0X(i2c)
-        # distance_sensor_A.set_address(0x30)  # address assigned should NOT be already in use
-        table_of_distance_sensors.insert(0,distance_sensor_A)
-        print("Check 4!")
+# initialize a list to be used for the array of VL53L0X sensors
+vl53 = []
 
-
+for i, power_pin in enumerate(xshut): # now change the addresses of the VL53L0X sensors
+    power_pin.value = True # turn on the VL53L0X to allow hardware check
+    # instantiate the VL53L0X sensor on the I2C bus & insert it into the "vl53" list
+    vl53.insert(i, VL53L0X(i2c))  # also performs VL53L0X hardware check
+    if i < len(xshut) - 1:
+        vl53[i].set_address(i + 0x30)  # address assigned should NOT be already in use
 
 def detect_range(count=5):
     """take count=5 samples"""
     while count:
-        for index, sensor in enumerate(table_of_distance_sensors):
-            print("Check 5!")
+        for index, sensor in enumerate(vl53):
             print("Sensor {} Range: {}mm".format(index + 1, sensor.range))
         time.sleep(1.0)
         count -= 1
+
 
 print(
     "Multiple VL53L0X sensors' addresses are assigned properly\n"
     "execute detect_range() to read each sensors range readings"
 )
+
+detect_range()
