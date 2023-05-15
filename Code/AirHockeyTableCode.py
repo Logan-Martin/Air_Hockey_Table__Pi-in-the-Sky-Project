dcddb9eef7b0970bf77c37172a1f523b8a1fb0fa
Logan-Martin <lmartin27@charlottesvilleschools.org>
2023-05-15 10:47:59 -0400

    sync

--- Code/AirHockeyTableCode.py
+++ Code/AirHockeyTableCode.py
@@ -9,10 +9,10 @@ from CircuitPython_LCDFolder.lcd.i2c_pcf8574_interface import I2CPCF8574Interfac
 # http://www.penguintutor.com/electronics/pico-lcd 
 # Make sure to have at least 5v. Pico only gives 3 volts. use battery back.
 
-i2c_address = 0x3f
+i2c_address = 0x29
 cols = 16
 rows = 2
-i2c_bus_0 = busio.I2C(board.GP1, board.GP0) # 1 rn
+i2c_bus_0 = busio.I2C(board.GP15, board.GP14) # 1 rn
 interface = I2CPCF8574Interface(i2c_bus_0, i2c_address)
 lcd = LCD(interface, num_rows=rows, num_cols=cols)
 
@@ -43,11 +43,12 @@ player2 = {
 # Distance Sensor Stuff:
 distanceSensor_player1 = VL53L0X(i2c_bus_0)
 # declare the singleton variable for the default I2C bus:
-i2c_bus_1 = busio.I2C(board.GP15,board.GP14)
+i2c_bus_1 = busio.I2C(board.GP1,board.GP0)
 distanceSensor_player2 = VL53L0X(i2c_bus_1)
 
 distanceReq_min = 12 ## going below 12 apparently breaks the sensor?
-distanceReq_max = 45 ## 50mm
+distanceReq_max = 24 ## 50mm
+distanceRequirmentToReset = 50
 
 time_before_autoreset = 5
 def playerWonFunction(whoScored):
@@ -95,14 +96,15 @@ while True:
        resetButtonWasPressed = False
 
 # Scoring w/ Distance Sensors:
-    if distanceSensor_player1.distance < distanceReq_min and distanceSensor_player1.distance > 8 and player1["playerWonThisRound"] == False and player1_can_score == True:
+    if distanceSensor_player1.distance < distanceReq_min and distanceSensor_player1.distance > distanceReq_max and player1["playerWonThisRound"] == False and player1_can_score == True:
         player1_can_score = False
         playerScoredFunction(player1)
-    if distanceSensor_player1.distance > distanceReq_max and player1_can_score == False:
+    if distanceSensor_player1.distance > distanceRequirmentToReset and player1_can_score == False:
         player1_can_score = True
 
-    if distanceSensor_player2.distance < distanceReq_min and distanceSensor_player2.distance > 8 and player2["playerWonThisRound"] == False and player2_can_score == True:
+
+    if distanceSensor_player2.distance < distanceReq_min and distanceSensor_player2.distance > distanceReq_max and player2["playerWonThisRound"] == False and player2_can_score == True:
         player2_can_score = False
         playerScoredFunction(player2)
-    if distanceSensor_player2.distance > distanceReq_max and player2_can_score == False:
+    if distanceSensor_player2.distance > distanceRequirmentToReset and player2_can_score == False:
         player2_can_score = True
