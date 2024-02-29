2af0396b9e35cc68af290171e2b4a6cb10820646
Logan Martin <71342159+Logan-Martin@users.noreply.github.com>
2024-02-29 09:40:11 -0500

    Update just_LCD_code.py
    working on another project this year but came back to this as I improved this code. Well, last year it was just the search for I2C code for some reason, but yeah. The added notes should help people.

--- Code/just_LCD_code.py
+++ Code/just_LCD_code.py
@@ -1,20 +1,33 @@
-
-import time # type: ignore
 import board # type: ignore
+import digitalio  # type: ignore
+import time # type: ignore
 import busio  # type: ignore
+from digitalio import DigitalInOut # type: ignore
 
-i2c = busio.I2C(board.GP15,board.GP14) 
+from CircuitPython_LCD.lcd.lcd import LCD, CursorMode  # type: ignore
+from CircuitPython_LCD.lcd.i2c_pcf8574_interface import I2CPCF8574Interface  # type: ignore
+# https://github.com/dhalbert/CircuitPython_LCD
+# when downloading, the name of the folder was "CircuitPython_LCD-main", change that name of the folder and make sure it matches the above name.
 
-while not i2c.try_lock():
-    pass
+# http://www.penguintutor.com/electronics/pico-lcd 
+# Make sure to have at least 5v for LCD. Use battery pack or use VBUS. Note: VBUS has no volts when the pico's USB is unplugged.
 
-try:
-    while True:
-        print(
-            "I2C addresses found:",
-            [hex(device_address) for device_address in i2c.scan()],
-        )
-        time.sleep(2)
+i2c_address = 0x27 # check the i2c address of your specific device
+cols = 16
+rows = 2
+i2c_bus_0 = busio.I2C(board.GP11, board.GP10) # make sure to check (SLC, SCA)
+interface = I2CPCF8574Interface(i2c_bus_0, i2c_address)
+lcd = LCD(interface, num_rows=rows, num_cols=cols)
 
-finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
-    i2c.unlock()
+
+lcd.print("abc ")
+lcd.print("This is quite long and will wrap onto the next line automatically.")
+
+lcd.clear()
+
+# Start at the second line, fifth column (numbering from zero).
+lcd.set_cursor_pos(1, 4)
+lcd.print("Here I am")
+
+# Make the cursor visible as a line.
+lcd.set_cursor_mode(CursorMode.LINE)
